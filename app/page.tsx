30cd27338336d4a0006b6ed603ac7a4c0ec43276
Alan <alankiris@gmail.com>
2023-07-16 18:51:32 +0300

    Main page rework

--- app/page.tsx
+++ app/page.tsx
@@ -7,86 +7,110 @@ import { Button } from '@/components/ui/button'
 import { Moon, MoveDown, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import { Textarea } from '@/components/ui/textarea'
+import { useImmer } from 'use-immer'
+import { convertCyrillicToLatin } from '@/lib/utils'
+import { useEffect } from 'react'
 
 export default function Home() {
 	const { setTheme } = useTheme()
 
-	return (
-		<main className="w-screen h-screen">
-			<nav className={'text-white py-2 flex justify-center'}>
-				<div className={'flex justify-between max-w-5xl w-full'}>
-					<div className={'flex gap-5 items-center'}>
-						<h4 className={'scroll-m-20 text-xl font-semibold tracking-tight text-black dark:text-white'}>
-							Пацанский translate
-						</h4>
-						{/*<Link*/}
-						{/*	to="home"*/}
-						{/*	spy={true}*/}
-						{/*	smooth={true}*/}
-						{/*	duration={500}*/}
-						{/*	activeClass={'text-white'}*/}
-						{/*	className={'text-muted-foreground'}*/}
-						{/*>*/}
-						{/*	Home*/}
-						{/*</Link>*/}
-						{/*<Link*/}
-						{/*	to="translate"*/}
-						{/*	spy={true}*/}
-						{/*	smooth={true}*/}
-						{/*	duration={500}*/}
-						{/*	activeClass={'text-white'}*/}
-						{/*	className={'text-muted-foreground'}*/}
-						{/*>*/}
-						{/*	Translate*/}
-						{/*</Link>*/}
-						{/*<Link*/}
-						{/*	to="examples"*/}
-						{/*	spy={true}*/}
-						{/*	smooth={true}*/}
-						{/*	duration={500}*/}
-						{/*	activeClass={'text-white'}*/}
-						{/*	className={'text-muted-foreground'}*/}
-						{/*>*/}
-						{/*	Examples*/}
-						{/*</Link>*/}
-					</div>
+	const [value, setValue] = useImmer('')
+	const [result, setResult] = useImmer<string | null>('')
 
-					<DropdownMenu>
-						<DropdownMenuTrigger asChild>
-							<Button size="icon">
-								<Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-								<Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-								<span className="sr-only">Toggle theme</span>
-							</Button>
-						</DropdownMenuTrigger>
-						<DropdownMenuContent align="end">
-							<DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
-							<DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
-							<DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
-						</DropdownMenuContent>
-					</DropdownMenu>
+	const handleChangeValue = (value: string) => {
+		setValue(value)
+		setResult(convertCyrillicToLatin(value))
+	}
+
+	useEffect(() => {
+		console.log({ result })
+	}, [result])
+
+	return (
+		<main className="min-w-screen min-h-screen flex flex-col items-center gap-10">
+			<nav className={'flex justify-between max-w-5xl w-full py-2 border-b-[1px]'}>
+				<div className={'flex gap-5 items-center'}>
+					<h4 className={'scroll-m-20 text-xl font-semibold tracking-tight text-black dark:text-white'}>
+						Пацанский translate
+					</h4>
+					{/*<Link*/}
+					{/*	to="home"*/}
+					{/*	spy={true}*/}
+					{/*	smooth={true}*/}
+					{/*	duration={500}*/}
+					{/*	activeClass={'text-white'}*/}
+					{/*	className={'text-muted-foreground'}*/}
+					{/*>*/}
+					{/*	Home*/}
+					{/*</Link>*/}
+					{/*<Link*/}
+					{/*	to="translate"*/}
+					{/*	spy={true}*/}
+					{/*	smooth={true}*/}
+					{/*	duration={500}*/}
+					{/*	activeClass={'text-white'}*/}
+					{/*	className={'text-muted-foreground'}*/}
+					{/*>*/}
+					{/*	Translate*/}
+					{/*</Link>*/}
+					{/*<Link*/}
+					{/*	to="examples"*/}
+					{/*	spy={true}*/}
+					{/*	smooth={true}*/}
+					{/*	duration={500}*/}
+					{/*	activeClass={'text-white'}*/}
+					{/*	className={'text-muted-foreground'}*/}
+					{/*>*/}
+					{/*	Examples*/}
+					{/*</Link>*/}
 				</div>
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button size="icon">
+							<Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+							<Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+							<span className="sr-only">Toggle theme</span>
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="end">
+						<DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
+						<DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
+						<DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
 			</nav>
-			<Separator className={'mb-10'} />
-			<div className={'flex justify-center w-full mb-40'}>
-				<div className={'max-w-5xl flex items-center flex-col'}>
-					<Image src={Main} alt={''} className={'mx-auto'} />\
-					<div className={'translate-y-[-60px] relative bg-primary-foreground rounded-lg'}>
-						<h1
-							className={
-								'scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl  text-center text-black dark:text-white '
-							}
-						>
-							Переводчик для настоящих геймеров и пацанов
-						</h1>
-					</div>
-					<Button className={'border-2'}>
-						Поехали <MoveDown />
-					</Button>
+			<div className="flex max-w-5xl w-full gap-5">
+				<div className="flex justify-center items-center flex-[0.5]">
+					<Image src={Main} alt={''} className={'mx-auto'} />
+				</div>
+				<div className="flex flex-col gap-5 flex-[0.5] ">
+					<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+						Переводчик для настоящих геймеров и пацанов
+					</h1>
+					<h4 className="scroll-m-20 text-xl tracking-tight h-full">
+						Помнишь те прекрасные времена когда после смены на заправке заходил на свой любимый сервер в
+						каэсочке и ебашил с своим фирменным ником NAGIBATOR007? Появялось желание вернуть ту
+						уникальность твоего никнейма как в старые времена? Тогда этот сайт твой лучший друг!
+					</h4>
+					<Button className={'border-2 mt-auto'}>Поехали</Button>
 				</div>
 			</div>
-			<div className={''}>
-				<h4 className={'scroll-m-20 text-xl font-semibold tracking-tight'}>Переводчик</h4>
+			<Separator className={'max-w-5xl'} />
+
+			<div className={'flex gap-5 max-w-5xl w-full'}>
+				<Textarea
+					placeholder={'Пиши сюда'}
+					className="w-full"
+					value={value}
+					onChange={event => handleChangeValue(event.target.value)}
+				/>
+				<Textarea
+					placeholder={'Получай тут'}
+					className="w-full"
+					value={result !== null ? result : 'Error!'}
+					readOnly
+				/>
 			</div>
 		</main>
 	)
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import { letterDictionary } from '@/lib/data'
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
@@ -9,4 +10,20 @@ export const getRandomIn = (min: number, max: number) => {
 	min = Math.ceil(min)
 	max = Math.floor(max)
 	return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+export const convertCyrillicToLatin = (text: string) => {
+	let convertedText = ''
+	try {
+		for (let i = 0; i < text.length; i++) {
+			const char = text[i]
+			const convertedChar = letterDictionary[char] || char
+			convertedText += convertedChar
+		}
+	} catch (error) {
+		console.log({ error })
+		return null
+	}
+
+	return convertedText
 }
